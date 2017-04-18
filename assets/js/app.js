eaf238ed54d3a5ce67a977157ce21e141e0b22f5
Fiorella Cisneros <fiorella.0624@gmail.com>
2017-04-18 16:11:27 -0500

    js

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+var contenedor = document.getElementById("contenedor");
+
+var div = document.createElement('div');
+
+	div.setAttribute('class', 'contprincipal');
+	contenedor.appendChild(div);
+
+	contprincipal.style.position = "relative";
+	contprincipal.style.background = "#ed462f";
+	contprincipal.style.width = "600px";
